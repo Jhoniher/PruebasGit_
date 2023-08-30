12e6520ca1425a4f852d03c66f0f001ef942f49f
Jhony Alpala <jhoniher95@gmail.com>
2023-08-30 14:09:33 -0500

    Se agregaron variables de numeros enteros

---
+++ DatosGeometricos.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package datosgeometricos;
+import java.util.Scanner;
+public class DatosGeometricos {
+
+    
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+    }
+    
+}
